0d4591c10f5293a0cfda2258c88121ea61e43237
smg <alsrl8@naver.com>
2022-10-06 15:40:23 +0900

    [FEAT] add Mercenary class with new Enum classes

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('hello world!')
+from typing import Union
+from enum import Enum
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class RoleEnum(str, Enum):
+    protector = 'Protector'
+    fighter = 'Fighter'
+    caster = 'Caster'
+
+
+class RarityEnum(str, Enum):
+    rare = 'Rare'
+    epic = 'Epic'
+    legendary = 'Legendary'
+
+
+class Mercenary(BaseModel):
+    name: str
+    role: RoleEnum
+    rarity: RarityEnum
+    # TODO minion type 추가
+    # TODO faction(진영) 어떻게 처리할지
+
+
+@app.get("/")
+def read_root():
+    return {"Hello", "World"}
