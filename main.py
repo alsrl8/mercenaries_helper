0efd3a747e4fd4c6e5d2de8ce7a8a9b9e3937cf2
smg <alsrl8@naver.com>
2022-10-13 23:26:16 +0900

    [FEAT] Remove unused methods

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from fastapi import FastAPI, Depends, HTTPException
 from sqlalchemy.orm import Session
 from typing import List
 
-import utils
 from sql_app import crud, models, schemas
 from sql_app.database import SessionLocal, engine
 
@@ -18,40 +17,6 @@ def get_db():
         yield db
     finally:
         db.close()
-
-
-@app.post("/users/", response_model=schemas.User)
-def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = crud.get_user_by_email(db, email=user.email)
-    if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
-
-    return crud.create_user(db=db, user=user)
-
-
-@app.get("/users/", response_model=List[schemas.User])
-def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    users = crud.get_users(db, skip=skip, limit=limit)
-    return users
-
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
-    if db_user is None:
-        raise HTTPException(status_code=404, detail="User not found")
-    return db_user
-
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Item)
-def create_item_for_user(user_id: int, item: schemas.ItemCreate, db: Session = Depends(get_db)):
-    return crud.create_user_item(db=db, item=item, user_id=user_id)
-
-
-@app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    items = crud.get_items(db, skip=skip, limit=limit)
-    return items
 
 
 @app.get("/mercenaries/", response_model=List[schemas.Mercenary])
@@ -78,10 +43,7 @@ def str2bool(v):
 
 
 def main(args):
-    utils.write_all_mercenary_names()
-    mercenary_names = utils.read_all_mercenary_names_from_local()
-    for name in mercenary_names:
-        utils.write_mercenary_info(name)
+    pass
 
 
 if __name__ == '__main__':
