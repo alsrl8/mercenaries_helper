06a95e33d93df3281d384d154582a8985cdd78ad
smg <alsrl8@naver.com>
2022-10-13 23:16:30 +0900

    [FEAT] Remove unused methods

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -3,14 +3,6 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def create_user_item(db: Session, item: schemas.ItemCreate, user_id: int):
-    db_item = models.Item(**item.dict(), owner_id=user_id)
-    db.add(db_item)
-    db.commit()
-    db.refresh(db_item)
-    return db_item
-
-
 def get_mercenaries(db: Session):
     return db.query(models.Mercenary).all()
 
