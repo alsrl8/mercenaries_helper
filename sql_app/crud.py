c1da72566d12403f8a03226d8ed246fcbc5c2164
smg <alsrl8@naver.com>
2022-10-17 00:58:32 +0900

    [FEAT] Add method for storing new equipment and getting mercenary with its equipments

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -8,14 +8,14 @@ def get_mercenaries(db: Session):
 
 
 def get_mercenary(db: Session, id: int = 0, name: str = ''):
-    if id and name:
-        return db.query(models.Mercenary).filter(models.Mercenary.id == id).filter(models.Mercenary.name == name).first()
-    elif id:
-        return db.query(models.Mercenary).filter(models.Mercenary.id == id).first()
-    elif name:
-        return db.query(models.Mercenary).filter(models.Mercenary.name == name).first()
-    else:
+    if not id and not name:
         return None
+    data = db.query(models.Mercenary).join(models.Equipment).filter(models.Mercenary.id == models.Equipment.owner_id)
+    if id:
+        data = data.filter(models.Mercenary.id == id)
+    if name:
+        data = data.filter(models.Mercenary.name == name)
+    return data.first()
 
 
 def create_mercenary(db: Session, mercenary: schemas.MercenaryCreate):
@@ -30,3 +30,15 @@ def create_mercenary(db: Session, mercenary: schemas.MercenaryCreate):
     db.commit()
     db.refresh(db_mercenary)
     return db_mercenary
+
+
+def create_equipment(db: Session, equipment: schemas.EquipmentCreate):
+    db_equipment = \
+        models.Equipment(name=equipment.name,
+                         desc=equipment.desc,
+                         owner_id=equipment.mercenary_id
+                         )
+    db.add(db_equipment)
+    db.commit()
+    db.refresh(db_equipment)
+    return db_equipment
