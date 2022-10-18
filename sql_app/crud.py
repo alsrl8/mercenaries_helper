7e1b8cf3906031501c139de296dd36e0a58ea94d
smg <alsrl8@naver.com>
2022-10-19 01:55:52 +0900

    [FEAT] Add new functions that select and update equipment

--- sql_app/crud.py
+++ sql_app/crud.py
@@ -18,6 +18,20 @@ def get_mercenary(db: Session, id: int = 0, name: str = ''):
     return data.first()
 
 
+def get_equipment(db: Session, id: int = 0, name: str = '', owner_id: int = 0):
+    if not id and not name:
+        return None
+    if owner_id:
+        data = db.query(models.Equipment).join(models.Mercenary).filter(models.Mercenary.id == owner_id)
+    else:
+        data = db.query(models.Equipment)
+    if id:
+        data = data.filter(models.Equipment.id == id)
+    if name:
+        data = data.filter(models.Equipment.name == name)
+    return data.first()
+
+
 def create_mercenary(db: Session, mercenary: schemas.MercenaryCreate):
     db_mercenary = \
         models.Mercenary(name=mercenary.name,
@@ -35,10 +49,21 @@ def create_mercenary(db: Session, mercenary: schemas.MercenaryCreate):
 def create_equipment(db: Session, equipment: schemas.EquipmentCreate):
     db_equipment = \
         models.Equipment(name=equipment.name,
-                         desc=equipment.desc,
-                         owner_id=equipment.mercenary_id
+                         text=equipment.text,
                          )
     db.add(db_equipment)
     db.commit()
     db.refresh(db_equipment)
     return db_equipment
+
+
+def update_equipment(db: Session, name: str, new_equipment: schemas.Equipment):
+    find_equipment = get_equipment(db=db, name=name)
+    if new_equipment.name:
+        find_equipment.name = new_equipment.name
+    if new_equipment.text:
+        find_equipment.text = new_equipment.text
+    if new_equipment.owner_id:
+        find_equipment.owner_id = new_equipment.owner_id
+    db.commit()
+    db.refresh(find_equipment)
