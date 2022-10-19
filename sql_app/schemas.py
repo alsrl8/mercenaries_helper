6d16e0259b07e14b920c37e5278d2fc256327e86
smg <alsrl8@naver.com>
2022-10-19 16:47:21 +0900

    [FEAT] Add ability related schema

--- sql_app/schemas.py
+++ sql_app/schemas.py
@@ -3,6 +3,26 @@ from sql_app.enums import Role, Rarity, MinionType, Faction
 from typing import List
 
 
+class AbilityBase(BaseModel):
+    name: str
+    speed: int
+    cooldown: int
+    spell_school: str
+    text: str
+
+
+class AbilityCreate(AbilityBase):
+    pass
+
+
+class Ability(AbilityBase):
+    id: int
+    owner_id: int
+
+    class Config:
+        orm_mode = True
+
+
 class EquipmentBase(BaseModel):
     name: str
     text: str
